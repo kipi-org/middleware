4675052ea492a2171e358a2756e54bbf6bcc1acb
Savva Marin <masadfff@mail.ru>
2024-02-10 19:46:01 +0300

    Add cors

--- src/main/kotlin/kipi/Application.kt
+++ src/main/kotlin/kipi/Application.kt
@@ -10,6 +10,14 @@ import com.fasterxml.jackson.module.kotlin.jsonMapper
 import com.fasterxml.jackson.module.kotlin.kotlinModule
 import io.ktor.http.*
 import io.ktor.http.ContentType.Application.Json
+import io.ktor.http.HttpHeaders.AcceptEncoding
+import io.ktor.http.HttpHeaders.Authorization
+import io.ktor.http.HttpHeaders.ContentType
+import io.ktor.http.HttpHeaders.Origin
+import io.ktor.http.HttpHeaders.UserAgent
+import io.ktor.http.HttpMethod.Companion.Get
+import io.ktor.http.HttpMethod.Companion.Options
+import io.ktor.http.HttpMethod.Companion.Post
 import io.ktor.http.HttpStatusCode.Companion.Forbidden
 import io.ktor.http.HttpStatusCode.Companion.Unauthorized
 import io.ktor.http.HttpStatusCode.Companion.UnprocessableEntity
@@ -32,11 +40,10 @@ fun main() {
 fun Application.init() {
     install(CORS) {
         anyHost()
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Post)
-        allowMethod(HttpMethod.Get)
-        allowHeader(HttpHeaders.ContentType)
-        allowHeader(HttpHeaders.Authorization)
+        allowMethod(Options)
+        allowMethod(Post)
+        allowMethod(Get)
+        allowHeaders { true }
     }
     val mapper = jsonMapper {
         disable(WRITE_DATES_AS_TIMESTAMPS)
