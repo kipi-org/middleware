d671f8dd21c629bc0184a260ae5784d41d101633
Savva Marin <masadfff@mail.ru>
2024-02-19 23:56:32 +0300

    Edit helper

--- src/main/kotlin/kipi/services/HelperService.kt
+++ src/main/kotlin/kipi/services/HelperService.kt
@@ -2,6 +2,7 @@ package kipi.services
 
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.http.ContentType.Application.Json
@@ -13,12 +14,15 @@ class HelperService(
     private val client: HttpClient
 ) {
     suspend fun getHelperAdvice(
-        userId: Long,
-        message: String,
-        transactions: List<Transaction>
+        userId: Long, message: String, transactions: List<Transaction>
     ): HelperResponse {
         val response = client.post {
             url { path("/api/Chat/SendMessage") }
+            timeout {
+                this.socketTimeoutMillis = 30000
+                this.connectTimeoutMillis = 30000
+                this.requestTimeoutMillis = 30000
+            }
             contentType(Json)
             header("ApiKey", "f2f572d7-ce14-4008-84d6-e2ce170d6f85")
             setBody(HelperRequest(userId, message, transactions))
